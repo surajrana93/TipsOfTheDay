03c748f4e489deaa89265cb85261c40be7f8da33
Suraj Rana <surajrana93@gmail.com>
2021-09-26 11:41:23 +0530

    Update Read.py

--- Read.py
+++ Read.py
@@ -5,17 +5,15 @@ import time
 
 lines = 'lines'
 
-file = open('C:\Workspace\Experiment\Reminder\json.txt', 'rb+')
-seenTips = open('C:\Workspace\Experiment\Reminder\seenTips.txt', 'rb+')
-
-data = json.load(file)
-file.close()
-
 emptyFile = os.stat(
       'C:\Workspace\Experiment\Reminder\seenTips.txt').st_size == 0
 
 sourceEmptyFile = os.stat(
        'C:\Workspace\Experiment\Reminder\json.txt').st_size == 0
+
+
+seenTips = open('C:\Workspace\Experiment\Reminder\seenTips.txt', 'rb+')
+
 
 if(emptyFile != True):
         seenData = json.load(seenTips)
@@ -30,6 +28,10 @@ if(sourceEmptyFile == True):
         seenData = []
         seenTips = open('C:\Workspace\Experiment\Reminder\seenTips.txt', 'w')
         seenTips.close()
+
+file = open('C:\Workspace\Experiment\Reminder\json.txt', 'rb+')
+data = json.load(file)
+file.close()      
 
 tipsList = []
 
@@ -52,7 +54,9 @@ seenTips = open('C:\Workspace\Experiment\Reminder\seenTips.txt', 'w')
 json.dump(seenData, seenTips)
 
 file = open('C:\Workspace\Experiment\Reminder\json.txt', 'w')
-json.dump(data, file)
+
+if len(data) !=0:
+        json.dump(data, file)
 
 seenTips.close()
 file.close()
